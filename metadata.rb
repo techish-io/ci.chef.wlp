6b384b3009f99e864fbcfb51b40f7bcb7271023e
Jarek Gawor <jgawor@gmail.com>
2013-10-02 13:54:16 -0400

    update maintainer_email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'wlp'
 maintainer       'IBM'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email ''
 license          'Apache 2.0'
 description      'Installs WebSphere Application Server Liberty Profile'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
